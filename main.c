dd89e52cf1d22109f231d525a15137acffb17ac5
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-19 12:46:02 -0700

    execve funciona pero no se aplico el fork

--- main.c
+++ main.c
@@ -1,21 +1,47 @@
 #include "main.h"
+
+void sig_handler(int signo)
+{
+    printf("received EXIT\n");
+}
+
+
 int  main(void)
 {
-	char *buff;
+	char c[1024];
+	char *buff = NULL;
 	char *token;
+	char *av[1024];
+	int i = 0, x = 0;
 	size_t len = 0;
-	ssize_t read;
+	ssize_t read = 0;
 
-	printf("$ ");
-	read = getline(&buff, &len, stdin);
-	if (read == -1)
-		return (1);
-	token = strtok(buff, " ");
-	while (token != NULL)
+	signal(SIGINT, sig_handler);
+
+	while(1)
 	{
-		printf("%s\n", token);
-		token = strtok(NULL, " ");
+		printf("$ ");
+		read = getline(&buff, &len, stdin);
+		if (read == -1)
+			break;
+		token = strtok(buff, " \n");
+		while (token != NULL)
+		{
+			av[i] = token;
+			printf("%s\n", av[i]);
+			i++;
+			token = strtok(NULL, " \n");
+		}
+		av[i] = NULL;
+
+		sprintf(c, "%s/%s", "/usr/bin", av[0]);
+		printf("Before execve\n");
+		if (execve(c, av, NULL) == -1)
+			break;
+
+		printf("After execve\n");
 	}
+	printf("\n");
 	free(buff);
 	return (0);
 }
--- main.h
+++ main.h
@@ -2,3 +2,5 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include<signal.h>
