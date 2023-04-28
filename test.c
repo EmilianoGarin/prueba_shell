90123a705887dbb496c22df43c32e6a8fbefc12c
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-27 21:11:42 -0700

    solucionando problemas v0.4

--- test.c
+++ test.c
@@ -1,12 +1,49 @@
 #include "main.h"
 char **splitpath(void)
 {
-	char *ubi = getenv("PATH");
+	char *token, *ubi = getenv("PATH");
 	char **pwd;
+	int x = 0, j = 0, i = 0, size = 0;
 
 	if (ubi == NULL)
 		exit(EXIT_FAILURE);
-	pwd = split_buff(ubi, ":");
+	for (i = 0; ubi[i] != '\0'; i++)
+		if (ubi[i] == ':')
+			size++;
+
+	pwd = malloc(sizeof(char*) * (size + 1));
+	if (pwd == NULL)
+		exit(0);
+
+	printf("%d \n", size);
+	pwd[0] = strdup(strtok(ubi, ":"));
+	
+	printf("%s   %p \n", pwd[0], pwd[0]);
+	for (i = 1; i < size; i++)
+	{
+		pwd[i] = strdup(strtok(NULL, ":"));
+		
+		printf("%s   %p \n", pwd[i], pwd[i]);
+	}
+	pwd[++i] = NULL;
+	printf("%d \n", i);
+	
+	/*	for (i = 0; ubi[i] != '\0'; i++)
+	{
+		j++;
+		if (ubi[i] == ':')
+		{
+			ubi[i] = '\0';
+			pwd[x][j] = ubi[i];
+			j = 0;
+			x++;
+		}
+		else
+			pwd[x][j] = ubi[i];
+	}
+	pwd[x][j + 1] = '\0';
+	pwd[x + 1] = NULL;
+*/
 	return (pwd);
 }
 
